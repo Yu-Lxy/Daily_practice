d5729867a9684afcb39456d5feacedb3a797d077
李逍禹 <lixy@cheerspublishing.com>
2020-08-24 11:00:42 +0800

    socket

---
+++ Socket.Io-chat-master/README.md
@@ -0,0 +1,14 @@
+# Socket.Io-chat
+
+### 安装
+
+*请先保证node环境，再安装以下*
+```
+npm i express
+npm i socket.io
+```
+
+### 运行
+```
+node index.js
+```
---
+++ Socket.Io-chat-master/index.js
@@ -0,0 +1,27 @@
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  
+  //响应某用户发送消息
+  socket.on('chat message', (msg) => {
+    console.log('chat message:' + msg)
+    // 广播给所有人
+    io.emit('chat message', msg)
+  })  
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+// 监听3000端口
+http.listen(3000, () => {
+  console.log('listening on *:3000')
+})
